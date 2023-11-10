baf38cca59b0d2cb6c6e08b8d6b80744dbf63d93
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-09 22:19:37 -0300

    refactor: some fixes

--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -20,7 +20,7 @@ export const CheckboxContainer = styled(Checkbox.Root, {
     backgroundColor: '$bonapaz300',
   },
 
-  '&:focus': {
+  '&:focus, &[data-state="checked"]': {
     border: '2px solid $bonapaz300',
   },
 })
